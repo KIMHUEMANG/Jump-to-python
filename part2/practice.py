939bdd11bcaaa49777d5798f6ca5d4f32c3b82e0
KIMHUEMANG <piyrw9754@gmail.com>
2022-10-10 00:36:25 +0900

    Part2

---
+++ part2/practice.py
@@ -0,0 +1,42 @@
+# 1. 자료형
+print(5)
+print(-10)
+print(3.14)
+print(1000)
+print(5+3)
+
+# 2. 문자열
+print('풍선')
+print("나비")
+print("ㅋㅋㅋㅋㅋ")
+print("ㅋ" * 9)
+
+# 3. boolean (참 / 거짓)
+print(5 > 10)
+print(5 < 10)
+print(True)
+print(False)
+print(not True) # false
+print(not False) # true
+
+# 4. 변수
+# 애완동물을 소개해 주세요
+animal = "고양이"
+name = "해피"
+age = 2
+hobby = "낮잠"
+is_adult = age >= 3
+
+
+print("우리집 "+animal +"의 이름은 " + name +"이예요")
+hobby = "낮잠"
+print("연탄이는 "+str(age)+"살이며, "+hobby+"을 아주 좋아해요")
+print("연탄이는 어른일까요? " ,str(is_adult))
+
+# 5. 주석
+
+# 응 이건 주석
+
+'''
+이렇게 하면 한번에 주석 처리 가능
+'''
