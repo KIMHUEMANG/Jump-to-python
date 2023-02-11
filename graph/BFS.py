d00d4037439f39221889ae5c677154389ba5abe0
aesperer <piyrw9754@gmail.com>
2023-02-11 22:47:23 +0900

    BFS

---
+++ graph/BFS.py
@@ -0,0 +1,34 @@
+from collections import deque
+import sys
+
+sys.setrecursionlimit(5000)
+input = sys.stdin.readline
+
+
+def bfs(graph, start, visited):
+    queue = deque([start])
+    visited[start] = True
+
+    while queue:
+        v = queue.popleft()
+        print(v)
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+
+visited = [False] * 10
+graph = {
+    1: [2, 5, 9],
+    2: [1, 3],
+    3: [2, 4],
+    4: [3],
+    5: [1, 6, 8],
+    6: [5, 7],
+    7: [6],
+    8: [5],
+    9: [1, 10],
+    10: [9]
+}
+bfs(graph, 1, visited)
