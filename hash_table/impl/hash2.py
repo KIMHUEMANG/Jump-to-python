1785dec3ef33d32a078d7e83565ddc93959454b6
aesperer <piyrw9754@gmail.com>
2023-03-21 20:47:14 +0900

    해쉬 테이블 2

---
+++ hash_table/impl/hash2.py
@@ -0,0 +1,22 @@
+
+hash_table = list([0 for i in range(8)])
+
+def get_key(data):
+    return hash(data)
+
+def hash_func(key):
+    return key % 8
+
+def save_data(data, value):
+    hash_address = hash_func(get_key(data))
+    hash_table[hash_address] = value
+
+def read_data(data):
+    hash_address = hash_func(get_key(data))
+    return hash_table[hash_address]
+
+save_data('Dave', '11111111111')
+save_data('Wave', '11112231111')
+save_data('Save', '11132323111')
+
+print(read_data('Dave'))
