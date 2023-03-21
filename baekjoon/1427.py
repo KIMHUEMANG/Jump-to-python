00dc6c5d4038757f6ef3a6abed5d246f401583b5
aesperer <piyrw9754@gmail.com>
2023-03-21 21:08:24 +0900

    [1427] 소트 인사이드

--- baekjoon/1427.py
+++ baekjoon/1427.py
@@ -1,8 +1,10 @@
-N = int(input())
-li = list(map(int, str(N)))
-
-li.sort(reverse=True)
-
-for i in list:
-    print(i, end ='')
-
+n = int(input())
+ 
+li = []
+for i in str(n):
+    li.append(int(i))
+    
+li.sort(reverse=True) 
+ 
+for i in li:
+    print(i,end='')
